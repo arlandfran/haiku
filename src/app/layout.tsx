b2b3a730b57a69ee7390615fa6291a01fe675f23
Arland Torres <arland.torres@outlook.com>
2023-11-02 06:27:59 +0000

    Fix extra attributes from server warning
    - https://github.com/vercel/next.js/discussions/22388#discussioncomment-6809043

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           "bg-background relative flex min-h-screen flex-col font-sans antialiased",
