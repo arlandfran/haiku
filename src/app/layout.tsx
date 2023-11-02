52e189d18ac47c7eb2ccf937dcad9fb5be68418e
Arland Torres <arland.torres@outlook.com>
2023-11-02 03:23:48 +0000

    Initialize shadcn-ui and add css for text on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans, GeistMono } from "geist/font";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Haiku",
@@ -15,7 +16,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${GeistSans.variable} ${GeistMono.variable} font-sans antialiased`}
+        className={cn(
+          "font-sans antialiased",
+          GeistSans.variable,
+          GeistMono.variable,
+        )}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-semibold">Haiku*</h1>
-      <p>
+      <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Haiku*
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
         *a form of Japanese poetry. A haiku expresses a single feeling or
         impression and contains three unrhymed lines of five, seven, and five
         syllables, respectively.
       </p>
-      <p className="font-mono">
+      <p className="font-mono leading-7 [&:not(:first-child)]:mt-6">
         calm as a river tranquility in my heart blue summer skies reign
       </p>
     </div>
